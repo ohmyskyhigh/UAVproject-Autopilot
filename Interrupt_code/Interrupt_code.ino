26d6bb271046a0514e456ded9ad4cafea2fdbb3c
Kahabdoul <camillematar@hotmail.fr>
2017-03-15 17:52:11 +0000

    interrupt code
    basic code for interface between receiver and arduino

---
+++ Interrupt_code/Interrupt_code.ino
@@ -0,0 +1,13 @@
+volatile int GEAR = 0;
+
+void setup() {
+  attachInterrupt(2, INTgear, RISING);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  if (GEAR == 1){
+    digitalWrite(13, HIGH);
+  }
+
+}
