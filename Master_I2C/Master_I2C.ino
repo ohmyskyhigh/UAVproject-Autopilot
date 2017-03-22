649d4067380f49e146bd9b6eb704accc39ae267a
ohmyskyhigh <miaorunkun@gmail.com>
2017-03-22 19:42:58 +0000

    Master/Slave Write/Read example code

--- Master_I2C/Master_I2C.ino
+++ Master_I2C/Master_I2C.ino
@@ -1,36 +1,30 @@
-#include <Wire.h>
 
-int x = 3.1415926;
-float y = 8.31;
-float c = 998.111;
-float f = 0.332;
+#include <Wire.h>
+char mi[100];
+int a = 0;
+int b = 552.32;
+int c = 98.110;
 
 
 void setup() {
-  Wire.begin();
+  Wire.begin(); // join i2c bus (address optional for master)   
+
   Serial.begin(9600);
-
 }
-
 
 void loop() {
-  char charA[10];
-  char charB[10];
-  char charC[10];
-  char charD[10];
-  dtostrf(x, 4, 3, charA);
-  dtostrf(y, 4, 3, charB);
-  dtostrf(c, 4, 3, charC);
-  dtostrf(f, 4, 3, charD);
-  Serial.print(charA);
-  Serial.println(charB);
-  Serial.println(charC);
-  Serial.println(charD);
-  Wire.beginTransmission(5);
-  //Try to split the following string 
-  //String stringOne =  String(5.698, 3);                                // using a float and the decimal 
-  Wire.write(x);
-  Wire.endTransmission();
+  String x = String(a);
+  String y = String(b);
+  String g = String(c);
+  String m = String(x + "," + y + "," + g);
+  m.toCharArray(mi, 100);
+  Serial.println(a);
+  Serial.println(mi);
+  Wire.beginTransmission(8); // transmit to device #8
+  Wire.write(mi);
+  Wire.endTransmission();    // stop transmitting
+  a = a + 1;
   delay(500);
 }
+
   
--- Slave_I2C/Slave_I2C.ino
+++ Slave_I2C/Slave_I2C.ino
@@ -1,24 +1,23 @@
+
+
 #include <Wire.h>
 
-
 void setup() {
-  Wire.begin(5);
-  Wire.onReceive(dataIN);
-  Serial.begin(9600);
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
+}
 
-}
-void dataIN(int numBytes){
-  while (1 < Wire.available()){
-    char c = Wire.read();
-    //String stringOne =  String('a');                                          // converting a constant char 
-    Serial.println(c);
-  }
-}
-      
 void loop() {
   delay(100);
-
 }
 
-
-
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  while (0 < Wire.available()) { // loop through all but the last 
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  Serial.println("");
+}
